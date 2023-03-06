8a56de44de70cc7d9989d15b89863f744831a2a9
parveen232 <parveen297m210@gmail.com>
2023-03-06 09:20:12 +0530

    Route Grouping

---
+++ app/(marketing)/home/page.tsx
@@ -0,0 +1,9 @@
+export default function MarketingHome() {
+    return (
+        <div>
+          <h1>This is group route</h1>
+          <p>The difference b/w putting parentheses around something and not is that it just ignored in the router</p>
+          <p>/home</p>
+        </div>
+    )
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <p>First one i.e MarketingHome will win and the reason is cuz that was the first one</p>
+}
