c469b6284ab09dbcffa61c1d0dae2ed41dde5fa0
parveen232 <parveen297m210@gmail.com>
2023-03-18 12:45:12 +0530

    Loading Page

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,12 +1,16 @@
-// const getData = async (slug) => {
-//     const post = await getDataFromCMS(slug);
-//     return post;
-// }
+const delay = (time) => new Promise((resolve) => {
+    setTimeout(() => resolve(1), time)
+})
+
+const getData = async (slug) => {
+    const post = await delay(3500);
+    return { slug };
+}
 
 export default async function BlogPost({ params }) {
     console.log(params);
-    // const { slug } = params;
-    // const post = await getData(slug);
+    const { slug } = params;
+    const post = await getData(slug);
 
-    return <h1>Post</h1>
+    return <h1>{post.slug}</h1>
 }
